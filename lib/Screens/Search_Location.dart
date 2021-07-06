5c36e876a78ffaf1f6551f9ff2473907f93cddd0
Atiq Rehman <atiqrehman3054@gmail.com>
2021-07-06 21:15:47 +0500

    add search screen

--- lib/Screens/Search_Location.dart
+++ lib/Screens/Search_Location.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:portfolio/Screens/Services.dart';
 
 class Search_Loc extends StatefulWidget {
-
-
   @override
   _Search_LocState createState() => _Search_LocState();
 }
@@ -11,12 +9,131 @@ class Search_Loc extends StatefulWidget {
 class _Search_LocState extends State<Search_Loc> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hoverColor: Colors.grey[100],
+                    focusColor: Colors.grey[100],
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.location_searching_sharp),
+                      color: Colors.black,
+                      onPressed: () {},
+                    ),
+                    hintText: 'Search_Location',
+                    fillColor: Colors.grey[300],
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.grey[200], width: 32.0),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.location_on_outlined),
+                          onPressed: () {}),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Current Location',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text('Proived Your Current Location'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Recent Searches',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    SizedBox(
+                      width: 120,
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {}),
+                  ],
+                ),
+                Column(
+                  children: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
 
-      body: Container(
-        
+
+                      scrollDirection: Axis.vertical,
+                      itemCount: 12,
+                      itemBuilder: (ctx, index) {
+                        return Align(child:
+                        Column(
+                          children: [
+                            Divider(
+                              thickness: 1,
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                    icon: Icon(Icons.location_on_outlined),
+                                    onPressed: () {}),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Current Location',
+                                      style: TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text('Proived Your Current Location'),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),);
+                      },
+
+
+
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-
     ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/Screens/Search_Location.dart';
 import 'package:portfolio/Screens/Services.dart';
 
 void main() {
@@ -11,8 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      home: Services(),
+      home: Search_Loc(),
     );
   }
 }
+
 
